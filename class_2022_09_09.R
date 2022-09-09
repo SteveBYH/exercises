74ea0f00d91e94afcc8712762e44a2cd0b50a87c
Colin Rundel <rundel@gmail.com>
2022-09-09 15:45:51 -0400

    class

---
+++ class_2022_09_09.R
@@ -0,0 +1,100 @@
+# Exercise 1
+
+
+json = list(
+  "firstName" = "John",
+  "lastName" = "Smith",
+  "age" = 25,
+  "address" = list(
+      "streetAddress" = "21 2nd Street",
+      "city" = "New York",
+      "state" = "NY",
+      "postalCode" = 10021
+  ),
+  "phoneNumber" = list(
+    list(
+      "type" = "home",
+      "number" = "212 555-1239"
+    ),
+    list(
+      "type" = "fax",
+      "number" = "646 555-4567"
+    ) 
+  )
+)
+
+jsonlite::fromJSON('{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25,
+  "address": 
+    {
+      "streetAddress": "21 2nd Street",
+      "city": "New York",
+      "state": "NY",
+      "postalCode": 10021
+    },
+  "phoneNumber": 
+    [ {
+      "type": "home",
+      "number": "212 555-1239"
+    },
+    {
+      "type": "fax",
+      "number": "646 555-4567"
+    } ]
+}', simplifyVector=FALSE)
+
+
+
+## Exercise 2
+
+report = function(x) {
+  UseMethod("report")
+}
+
+report.default = function(x) {
+  "This class does not have a method defined."
+}
+
+report.integer = function(x) {
+  "I'm an integer!"
+}
+
+report.double = function(x) {
+  "I'm a double!"
+}
+
+report.numeric = function(x) {
+  "I'm a numeric!"
+}
+
+class(1)  # numeric
+class(1L) # integer
+
+
+## Step 1
+
+report("a") # Error
+report(1)   # Double
+report(1L)  # Integer
+
+# => double or integer takes priority over numeric
+
+## Step 2
+
+rm("report.integer")
+report("a") # Error
+report(1)   # Double
+report(1L)  # Numeric
+
+# => Integer's mode is used?
+
+## Step 3
+
+rm("report.double")
+report("a") # Error    
+report(1)   # Numeric
+report(1L)  # Numeric
+
+
