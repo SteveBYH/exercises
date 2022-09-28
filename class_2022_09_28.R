952c5e292e2f3c295c4c3b1f4045a51e86f3f664
Colin Rundel <rundel@gmail.com>
2022-09-28 14:58:18 -0400

    class

---
+++ class_2022_09_28.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+library(palmerpenguins)
+
+## Exercise 1
+
+penguins %>%
+  filter(!is.na(sex)) %>%
+  mutate(
+    sex = forcats::as_factor(sex)
+  ) %>%
+  ggplot(
+    aes(
+      x = body_mass_g,
+      fill = species
+    )
+  ) + 
+    geom_density(alpha=0.5, color=NA) +
+    facet_wrap(~sex, ncol=1) +
+    labs(
+      color = "Species",
+      x = "Body mass (g)",
+      y = ""
+    )
+
+
+## Exercise 2
+
+
+penguins %>%
+  ggplot(
+    aes(
+      x = flipper_length_mm,
+      y = bill_length_mm,
+      color = species,
+      shape = species
+    )
+  ) + 
+  geom_point() +
+  geom_smooth(method="lm", se=FALSE) +
+  theme_minimal() +
+  scale_color_manual(values = c("darkorange", "purple", "cyan4")) +
+  labs(
+    title = expression(theta^2 - 2*lambda)
+  )
+
+
+
+
